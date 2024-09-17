aedd0e767118eaa8366311a4f0e8538e7589606d
goyalshivansh2805 <144095961+goyalshivansh2805@users.noreply.github.com>
2024-09-17 10:49:39 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,6 @@ app.use(cors());
 
 app.use(express.json());
 
-// app.use(logger);
-
 app.use(express.urlencoded({extended:true}));
 
 app.use("/api/tasks",require("./routes/api/tasks.js"));
@@ -27,7 +25,6 @@ app.use("*",(req,res)=>{
     res.status(404).json({message:"Page not found"});
 });
 
-app.use(errorHandler);
 
 mongoose.connection.on("open",()=>{
     console.log("Connected to Database...");
